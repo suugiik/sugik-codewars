131aaf85d992012640c6c187f5c7fc737494f523
sugik <suugiik01@gmail.com>
2024-05-25 11:37:26 +0700

    .

---
+++ 6kyu/baru.js
@@ -0,0 +1,21 @@
+function solution(str){
+    // if(str.length % 2 == 1) {
+    //     str += '_';     
+    // }
+
+    (str.length % 2 == 1) ? str += '_' : str;
+    return str.split(/(.{2})/).filter(Boolean)
+}
+
+console.log(solution('asdc'));
+
+// catatan:
+// split memisahkan string berdasarkan koma. kalau gak ada bisa dengan cara yang saya lakukan di atas
+// let str = "Halo,dunia,ini,adalah,contoh";
+// let arr = str.split(","); // Memisahkan string berdasarkan koma
+// console.log(arr); // Output: ["Halo", "dunia", "ini", "adalah", "contoh"]
+
+// fungsi filter(Boolean). menghapus golongan kosong
+// let arr = [0, 1, 2, "", "hello", null, undefined, NaN];
+// let filteredArr = arr.filter(Boolean);
+// console.log(filteredArr); // Output: [1, 2, "hello"]
